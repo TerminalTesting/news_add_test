8bfd8b0d7e53c97f1e602696a97f99628a20c9a6
TerminalTesting <testing@terminal.ru>
2014-10-03 11:51:56 +0600

    code refactoring

--- news_add_test.py
+++ news_add_test.py
@@ -9,16 +9,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class NewsAddTest(unittest.TestCase):
+
+    def setUp(self):
+        #delete old screenshot artifacts
+        os.system('find -iname \*.png -delete')
     
-    SITE = 'http://nsk.%s/' % os.getenv('SITE')
-    ARTSOURCE = '%sartifact/' % os.getenv('BUILD_URL')
-    driver = webdriver.Firefox()
+        self.SITE = 'http://nsk.%s/' % os.getenv('SITE')
+        self.ARTSOURCE = '%sartifact/' % os.getenv('BUILD_URL')
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(15)
 
-    os.system('find -iname \*.png -delete')
-
-    TEST_NEWS = {'header': 'AUTOTEST NEWS HEADER',
-                 'anons': 'Autotest news anons',
-                 'text': 'This is autotest made news. I hope it`s add alright'}
+        self.TEST_NEWS = {'header': 'AUTOTEST NEWS HEADER',
+                          'anons': 'Autotest news anons',
+                          'text': 'This is autotest made news. I hope it`s add alright'}
 
 
     def tearDown(self):
@@ -29,68 +32,109 @@ class NewsAddTest(unittest.TestCase):
         if sys.exc_info()[0]:   
             print sys.exc_info()[0]
 
+    def is_element_present(self, how, what, timeout=10):
+        """ Поиск элемента по локатору
+
+            По умолчанию таймаут 10 секунд, не влияет на скорость выполнения теста
+            если элемент найден, если нет - ждет его появления 10 сек
+            
+            Параметры:
+               how - метод поиска
+               what - локатор
+            Методы - атрибуты класса By:
+             |  CLASS_NAME = 'class name'
+             |  
+             |  CSS_SELECTOR = 'css selector'
+             |  
+             |  ID = 'id'
+             |  
+             |  LINK_TEXT = 'link text'
+             |  
+             |  NAME = 'name'
+             |  
+             |  PARTIAL_LINK_TEXT = 'partial link text'
+             |  
+             |  TAG_NAME = 'tag name'
+             |  
+             |  XPATH = 'xpath'
+                                             """
+	try:
+            return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((how, what)))
+	except:
+            print u'Элемент не найден'
+	    print 'URL: ', self.driver.current_url
+	    print u'Метод поиска: ', how
+	    print u'Локатор: ', what
+	    screen_name = '%d.png' % int(time.time())
+	    self.driver.get_screenshot_as_file(screen_name)
+	    print u'Скриншот страницы: ', self.ARTSOURCE + screen_name
+	    raise Exception('ElementNotPresent')
+
     def test_news_add(self):
         cnt=0
+
+        driver = self.driver
+        TEST_NEWS = self.TEST_NEWS
+        element = self.is_element_present
         
-        self.driver.get('%slogin' % self.SITE)
-        self.driver.find_element_by_id('username').send_keys(os.getenv('AUTH'))
-        self.driver.find_element_by_id('password').send_keys(os.getenv('AUTHPASS'))
-        self.driver.find_element_by_class_name('btn-primary').click()
-        self.driver.get('%sterminal/admin/' % self.SITE)
-        time.sleep(5)
-        #self.driver.find_element_by_partial_link_text(u'тестовый режим').click()
-        self.driver.get('%sterminal/admin/site/terminal/tnews/list' % self.SITE)
-        self.driver.find_element_by_link_text(u'Добавить новый').click()
+        driver.get('%slogin' % self.SITE)
+        element(By.ID, 'username').send_keys(os.getenv('AUTH'))
+        element(By.ID, 'password').send_keys(os.getenv('AUTHPASS'))
+        element(By.CLASS_NAME, 'btn-primary').click()
+        driver.get('%sterminal/admin/' % self.SITE)
+        element(By.LINK_TEXT, u'тестовый режим').click()
+        driver.get('%sterminal/admin/site/terminal/tnews/list' % self.SITE)
+        element(By.LINK_TEXT, u'Добавить новый').click()
 
         #добавляем заголовок новости
-        self.driver.find_element_by_css_selector('input[id*="_name"]').send_keys(self.TEST_NEWS['header'])
+        element(By.CSS_SELECTOR, 'input[id*="_name"]').send_keys(TEST_NEWS['header'])
 
         #берем текущую дату
         current = time.strftime("%Y/%m/%d").split('/')
 
-        startDate_year = self.driver.find_element_by_css_selector('select[id*="_startDate_year"]')
+        startDate_year = element(By.CSS_SELECTOR, 'select[id*="_startDate_year"]')
         for y in startDate_year.find_elements_by_tag_name('option'):
             if y.text == current[0]:#change to current year
 		y.click()
 
-	startDate_month = self.driver.find_element_by_css_selector('select[id*="_startDate_month"]')
+	startDate_month = element(By.CSS_SELECTOR, 'select[id*="_startDate_month"]')
 	for m in startDate_month.find_elements_by_tag_name('option'):
             if m.text == current[1]:#change to current month
 		m.click()
 
-	startDate_day = self.driver.find_element_by_css_selector('select[id*="_startDate_day"]')
+	startDate_day = element(By.CSS_SELECTOR, 'select[id*="_startDate_day"]')
 	for d in startDate_day.find_elements_by_tag_name('option'):
             if d.text == current[2]:#change to current day
 		d.click()
 
-	endDate_year = self.driver.find_element_by_css_selector('select[id*="_endDate_year"]')
+	endDate_year = element(By.CSS_SELECTOR, 'select[id*="_endDate_year"]')
 	for ey in endDate_year.find_elements_by_tag_name('option'):
             if ey.text == '2019':#change to maximal year parameter
 		ey.click()
 
         #выбираем Новосибирск, в последствии можно добавить проверку и по городам
-	self.driver.find_element_by_css_selector('input[id*="_cities_1"]').click()
+	element(By.CSS_SELECTOR, 'input[id*="_cities_1"]').click()
 
         #Заполняем анонс новости(сообщение выводящееся на странице новостей)
-	self.driver.find_element_by_css_selector('textarea[id*="_anounce"]').send_keys(self.TEST_NEWS['anons'])
+	element(By.CSS_SELECTOR, 'textarea[id*="_anounce"]').send_keys(TEST_NEWS['anons'])
 
 	#переключаемся на фрейм, т.к. контент ckeditor`а во фрейме
-	self.driver.switch_to_frame(self.driver.find_element_by_tag_name("iframe"))
+	driver.switch_to_frame(element(By.TAG_NAME, 'iframe'))
 	#добавляем скриптом текст новости в тег p
-	self.driver.execute_script('document.getElementsByTagName("p")[0].innerHTML = "%s";' % self.TEST_NEWS['text'])
+	driver.execute_script('document.getElementsByTagName("p")[0].innerHTML = "%s";' % TEST_NEWS['text'])
 
         #переключение в основной контент из фрейма
-	self.driver.switch_to_default_content()
+	driver.switch_to_default_content()
 	#сохранение данных
-	self.driver.find_element_by_class_name('btn-primary').click()
+	element(By.CLASS_NAME, 'btn-primary').click()
 
-	news_url = self.driver.current_url
+	news_url = driver.current_url
 
 	#переходим на страницу новостей, чтобы проверить текст анонса
-	self.driver.get('%snews/' % self.SITE)
+	driver.get('%snews/' % self.SITE)
 
 	#выбираем последнюю новость
-	last_news = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'news-list__item')))
+	last_news = element(By.CLASS_NAME, 'news-list__item')
 
 	#проверяем дату публикации на странице новостей
         if time.strftime("%d-%m-%Y") != last_news.find_element_by_tag_name('time').get_attribute('datetime').strip():
@@ -100,16 +144,16 @@ class NewsAddTest(unittest.TestCase):
             print '*'*80
 	
         #проверяем заголовок новости на странице новостей
-        if self.TEST_NEWS['header'] != last_news.find_element_by_tag_name('a').text:
+        if TEST_NEWS['header'] != last_news.find_element_by_tag_name('a').text:
             cnt += 1
-            print 'Некорректный заголовок новости, нужно - ', self.TEST_NEWS['header']
+            print 'Некорректный заголовок новости, нужно - ', TEST_NEWS['header']
             print 'Hа странице -', last_news.find_element_by_tag_name('a').text
             print '*'*80
 
         #проверяем анонс новости на странице новостей
-        if self.TEST_NEWS['anons'] != last_news.find_element_by_tag_name('p').text:
+        if TEST_NEWS['anons'] != last_news.find_element_by_tag_name('p').text:
             cnt += 1
-            print 'Некорректный анонс новости, нужно - ', self.TEST_NEWS['anons']
+            print 'Некорректный анонс новости, нужно - ', TEST_NEWS['anons']
             print 'Hа странице -', last_news.find_element_by_tag_name('p').text
             print '*'*80
 
@@ -117,7 +161,7 @@ class NewsAddTest(unittest.TestCase):
         last_news.find_element_by_tag_name('a').click()
 
         #ссылаемся на новый объект
-        last_news = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'article')))
+        last_news = element(By.TAG_NAME, 'article')
 
         #проверяем дату публикации на странице новости
         if time.strftime("%d-%m-%Y") != last_news.find_element_by_tag_name('time').get_attribute('datetime').strip():
@@ -127,27 +171,27 @@ class NewsAddTest(unittest.TestCase):
             print '*'*80
 	
         #проверяем заголовок новости на странице новости
-        if self.TEST_NEWS['header'] != last_news.find_element_by_tag_name('h1').text:
+        if TEST_NEWS['header'] != last_news.find_element_by_tag_name('h1').text:
             cnt += 1
             print 'Некорректный заголовок новости на странице новости, нужно - ', TEST_NEWS['header']
             print 'Hа странице -', last_news.find_element_by_tag_name('h1').text
             print '*'*80
 
         #проверяем анонс новости на странице новости
-        if self.TEST_NEWS['text'] != last_news.find_elements_by_tag_name('p')[1].text:
+        if TEST_NEWS['text'] != last_news.find_elements_by_tag_name('p')[1].text:
             cnt += 1
-            print 'Некорректный текст на странице новости, нужно - ', self.TEST_NEWS['text']
+            print 'Некорректный текст на странице новости, нужно - ', TEST_NEWS['text']
             print 'Hа странице -', last_news.find_elements_by_tag_name('p')[1].text
             print '*'*80
 
-        self.driver.get(news_url)
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, u'Удалить'))).click()
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input.btn.btn-danger'))).click()
+        driver.get(news_url)
+        element(By.LINK_TEXT, u'Удалить').click()
+        element(By.CSS_SELECTOR, 'input.btn.btn-danger').click()
 
         news_id = news_url[len(self.SITE):].split('/')[5]
 
         try:
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, news_id)))
+            element(By.LINK_TEXT, news_id)
             cnt += 1
             print 'Новость не удалилась'
             print '*'*80
